a10389fb2c8a261a5d7e5ee6b1f18957422b2376
Davi Hilário <davihilario23@gmail.com>
2025-10-19 22:09:03 -0300

    feat: testando fluxo

--- infra/main.tf
+++ infra/main.tf
@@ -13,6 +13,20 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "kp" {
+  key_name   = "key_19102025"
+  public_key = "~/.ssh/${tls_private_key.pk.public_key_openssh}"
+}
+resource "local_file" "ssh_key" {
+  filename        = "${aws_key_pair.kp.key_name}.pem"
+  content         = tls_private_key.pk.private_key_pem
+  file_permission = "0400"
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
